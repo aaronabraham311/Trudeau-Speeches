4f2ecf6e4064cfb3f9c6e7352601cc5a8a0be012
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-03 19:24:36 -0600

    Scrapped PM website; loaded data

--- src/crawler.py
+++ src/crawler.py
@@ -2,12 +2,20 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
-import os
+import time
 
 # Opening up connection and grabbing HTML file via Chrome
 url = 'https://pm.gc.ca/eng/news/speeches'
 browser = webdriver.Chrome()
 browser.get(url)
 
+# Delaying scrapper to prevent scrapper from closing too soon
+time.sleep(10)
+
+# Parsing the data via bs4
 html = browser.page_source
-print(html)
+soup = BeautifulSoup(html, 'html.parser')
+
+# Saving initial raw soup
+with open('../data/scrapped.txt', 'w') as file:
+	file.write(str(soup))
