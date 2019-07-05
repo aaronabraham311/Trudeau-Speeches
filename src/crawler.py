190b122ca024cf041983cb5f8e30dd4453c915c7
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-04 18:50:41 -0600

    Added command to click on article

--- src/crawler.py
+++ src/crawler.py
@@ -10,7 +10,7 @@ browser = webdriver.Chrome()
 browser.get(url)
 
 # Delaying scrapper to prevent scrapper from closing too soon
-time.sleep(10)
+time.sleep(2)
 
 # Parsing the data via bs4
 html = browser.page_source
@@ -19,3 +19,7 @@ soup = BeautifulSoup(html, 'html.parser')
 # Saving initial raw soup
 with open('../data/scrapped.txt', 'w') as file:
 	file.write(str(soup))
+
+# Clicking a speech box
+browser.find_element_by_css_selector(".views-row.views-row-1.pub1.default-on.clk").click()
+
