aa1db428259773387e8fe77353ee92e4bc69cf5b
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-05 20:57:10 -0600

    Cleaning code, implementing delays

--- src/crawler.py
+++ src/crawler.py
@@ -10,22 +10,18 @@ browser = webdriver.Chrome()
 browser.get(url)
 
 # Delaying scrapper to prevent scrapper from closing too soon
-time.sleep(2)
+browser.implicitly_wait(2)
 
 # Parsing the data via bs4
 html = browser.page_source
 soup = BeautifulSoup(html, 'html.parser')
-
-# Saving initial raw soup
-with open('../data/scrapped.txt', 'w') as file:
-	file.write(str(soup))
 
 # Creating loop to open up all divs with same class name
 article_list = browser.find_elements_by_css_selector(".views-row.pub1.default-on.clk")
 
 for article in article_list:
 	article.click()
-	time.sleep(1)
+	browser.implicitly_wait(2)
 
 	# Getting date
 	date = browser.find_element_by_class_name("date-display-single")
@@ -40,7 +36,3 @@ for article in article_list:
 	
 	for p in speech_div:
 		print(p.text)
-	
-	
-
-# Problem: only 10 articles can be found by css_selector
