d040ec8ab4780a4e8560d143b26b461f05d87bc5
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-06 18:40:46 -0600

    Making crawler script based on AJAX requests

--- src/crawler_ajax.py
+++ src/crawler_ajax.py
@@ -1,52 +1,51 @@
-# Libraries
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
+
+headers = {
+    'User-Agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
+}
 
 
-# Function to parse text
 def make_soup(url: str) -> BeautifulSoup:
-    res = requests.get(url)
+    res = requests.get(url, headers=headers)
     res.raise_for_status()
-    return BeautifulSoup(res.txt, 'html.parser')
+    print("soup success")
+    return BeautifulSoup(res.text, 'html.parser')
 
-# Function to get speeches via AJAX
+
 def fetch_speech_details(speech_id: str) -> str:
-    url = 'https://pm.gc.ca/eng/views/ajax?view_name=news_article&view_display_id=block&view_args={speech_id}'
+    url = 'https://pm.gc.ca/eng/views/ajax?view_name=news_article&view_display_id=block&view_args={id}'
+    url = url.format(id = speech_id)
 
-    res = requests.get(url)
+    print(url)
+
+    res = requests.get(url, headers=headers)
     res.raise_for_status()
 
     data = res.json()
     html = data[1]['data']
-
     soup = BeautifulSoup(html, 'html.parser')
     body = soup.select_one('.views-field-body')
-
+    print("fetch success")
     return str(body)
 
-# Function to scrape speeches
+
 def scrape_speeches(soup: BeautifulSoup) -> dict:
     speeches = []
-
-    for teaser in soup.select('teaser'):
+    for teaser in soup.select('.teaser'):
         title = teaser.select_one('.title').text.strip()
         speech_id = teaser['data-nid']
         speech_html = fetch_speech_details(speech_id)
-
         s = {
             'title': title,
-            'details' = speech_html
+            'details': speech_html
         }
-
         speeches.append(s)
-
     return speeches
-
-# Main function
 
 if __name__ == "__main__":
     url = 'https://pm.gc.ca/eng/news/speeches'
     soup = make_soup(url)
     speeches = scrape_speeches(soup)
+    from pprint import pprint
     pprint(speeches)
