b9c5ad42cae753b0c7a1a2b519ff0f9df3a22349
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-13 08:28:52 -0600

    Function to extract speech from MongoDB

--- src/speech_process.py
+++ src/speech_process.py
@@ -63,3 +63,12 @@ def prepare_text(text):
 
     return tokens, bigrams, trigrams
 
+# Function to get speech
+def extract_speech():
+    # Intializing client and navigating to MongoDB database
+    client = MongoClient('localhost', 27017)
+    db = client.trudeau_speeches
+    speeches = db.db_speeches
+
+    return speeches
+
